e61bca109ba4fbf2cacfe687209af16c57460710
Your Name <you@example.com>
2024-06-10 13:22:34 +0300

    local Change

---
+++ server_management/server_management/doctype/consultant_name/consultant_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, server_management and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Consultant Name', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ server_management/server_management/doctype/consultant_name/test_consultant_name.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, server_management and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestConsultantName(unittest.TestCase):
+	pass
